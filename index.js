057f2c6b466c28347beaf4220b70b65f41b38f1a
JewelHFahim <jewelhfahim@gmail.com>
2022-11-07 01:27:22 +0600

    done

--- index.js
+++ index.js
@@ -14,6 +14,23 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qez1k8e.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    console.log(authHeader);
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+           return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    }) 
+}
+
 async function run(){
     try{
         const serviceCollection = client.db('geniusCar').collection('services');
@@ -22,7 +39,7 @@ async function run(){
         // JWT Start
         app.post('/jwt', (req, res)=>{
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1d'});
             res.send({token});
         }) 
         // JWT End
@@ -51,8 +68,12 @@ async function run(){
             res.send(result);
         })
 
-        app.get('/orders', async(req, res)=>{
-            // console.log(req.headers.authorization);
+        app.get('/orders', verifyJWT, async(req, res)=>{
+            const decoded = req.decoded;
+            console.log('inside orders api:', decoded);
+            if(decoded.email !== req.query.email){
+               return res.status(401).send({message: 'unauthorized access'});
+            }
             const id = req.params.id;
             let query = {};
             if(req.query.email){
@@ -65,14 +86,14 @@ async function run(){
             res.send(orders)
         })
 
-        app.delete('/orders/:id', async(req, res)=>{
+        app.delete('/orders/:id', verifyJWT,  async(req, res)=>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const result = await orderCollection.deleteOne(query);
             res.send(result);
         })
 
-        app.patch('/orders/:id', async(req, res)=>{
+        app.patch('/orders/:id', verifyJWT,  async(req, res)=>{
             const id = req.params.id;
             const status = req.body.status;
             const query = {_id: ObjectId(id)};
